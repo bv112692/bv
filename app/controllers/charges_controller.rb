389c77401ffa4b3632cabab9bf7d0a25b5b9a0ef
Royce Negron <royce@bio-views.com>
2014-08-13 17:22:58 -0400

    Passing real parameters

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,29 @@
 class ChargesController < ApplicationController
 
-def create
-  # Amount in cents
-  @amount = 500
+  def create
 
-  customer = Stripe::Customer.create(
-    :email => 'example@stripe.com',
-    :card  => params[:stripeToken]
-  )
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :card  => params[:stripeToken]
+    )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => params[:amount],
+      :description => 'Growth Hacking Crash Course',
+      :currency    => 'usd'
+    )
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path
-end
+    purchase = Purchase.create(email: params[:stripeEmail], 
+      card: params[:stripeToken], amount: params[:amount],
+      description: charge.description, currency: charge.currency,
+      customer_id: customer.id, product_id: 1, uuid: SecureRandom.uuid)
+
+    redirect_to purchase 
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
+
 end
