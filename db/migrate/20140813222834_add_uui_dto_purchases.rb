720544627c2dff526fead114f1e6b56a6b2dc1be
Royce Negron <royce@bio-views.com>
2014-08-13 18:37:33 -0400

    Added security on purchase number

---
+++ db/migrate/20140813222834_add_uui_dto_purchases.rb
@@ -0,0 +1,4 @@
+class AddUuiDtoPurchases < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140813212458) do
+ActiveRecord::Schema.define(version: 20140813222834) do
 
   create_table "models", force: true do |t|
     t.string   "email",                  default: "", null: false
